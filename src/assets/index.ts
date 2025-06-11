92c569cc5ddcfa64340cd4122ba54a9794fe8731
EucTech <euccreative@gmail.com>
2025-06-11 12:57:16 -0400

    updated assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,78 +1,17 @@
-import black_star from "./images/black-star.png";
-import yellow_star from "./images/yellow-star.png";
-import purple_star from "./images/purple-star.png";
-import white_star from "./images/white-star.png";
-import rose_star from "./images/rose-star.png";
-import person1 from "./images/person1.png";
-import person2 from "./images/person2.png";
-import person3 from "./images/person3.png";
-import logo from "./images/logo.jpg";
-import logo1 from "./images/logo1.png";
-import hero_img from "./images/hero-img.png";
-import brand1 from "./images/brand1.png";
-import brand2 from "./images/brand2.png";
-import brand3 from "./images/brand3.png";
-import brand4 from "./images/brand4.png";
-import brand5 from "./images/brand5.png";
-import brand6 from "./images/brand6.png";
-import brand7 from "./images/brand7.png";
-import icon1 from "./images/icon1.png";
-import icon2 from "./images/icon2.png";
-import icon3 from "./images/icon3.png";
-import h1 from "./images/h1.png";
+
+import logo from "./images/logo.png";
+import h1  from "./images/h1.png";
 import h2 from "./images/h2.png";
 import h3 from "./images/h3.png";
-import manycolorbg from "./images/manycolorbg.png";
-import faqs from "./images/faqs.png";
-import game_hero_img from "./images/game-hero-img.png";
-import service from "./images/service.png";
-import w1 from "./images/w1.png";
-import c1 from "./images/c1.png";
-import l1 from "./images/l1.png";
-import l2 from "./images/l2.png";
-import l3 from "./images/l3.png";
-import l4 from "./images/l4.png";
-import l5 from "./images/l5.png";
-import l6 from "./images/l6.png";
-import l7 from "./images/l7.png";
+import h4 from "./images/h4.png";
 
 
 export const Images = {
-  black_star,
-  yellow_star,
-  purple_star,
-  white_star,
-  rose_star,
-  person1,
-  person2,
-  person3,
+ 
   logo,
-  logo1,
-  hero_img,
-  brand1,
-  brand2,
-  brand3,
-  brand4,
-  brand5,
-  brand6,
-  brand7,
-  icon1,
-  icon2,
-  icon3,
   h1,
   h2,
   h3,
-  manycolorbg,
-  faqs,
-  game_hero_img,
-  service,
-  w1,
-  c1,
-  l1,
-  l2,
-  l3,
-  l4,
-  l5,
-  l6,
-  l7,
+  h4,
+  
 };
