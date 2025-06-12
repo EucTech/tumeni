894e77216f9e95b58a70fb7f3569eb684c0a8490
EucTech <euccreative@gmail.com>
2025-06-12 11:39:44 -0400

    updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,6 +4,23 @@ import h1  from "./images/h1.png";
 import h2 from "./images/h2.png";
 import h3 from "./images/h3.png";
 import h4 from "./images/h4.png";
+import h5 from "./images/h5.png";
+import h6 from "./images/h6.png";
+import h7 from "./images/h7.png";
+import h8 from "./images/h8.jpg";
+import shopify from "./images/shopify.png";
+import pay from "./images/pay.png";
+import icon1 from "./images/icon1.png";
+import icon2 from "./images/icon2.png";
+import icon3 from "./images/icon3.png";
+import icon4 from "./images/icon4.png";
+import agent from "./images/agent.png";
+import whyimage from "./images/whyimage.png";
+import how1 from "./images/how1.png";
+import brands from "./images/brands.png";
+import star from "./images/star.png";
+import faqs from "./images/faqs.png";
+import c from "./images/c.png";
 
 
 export const Images = {
@@ -13,5 +30,23 @@ export const Images = {
   h2,
   h3,
   h4,
+  h5,
+  h6,
+  h7,
+  h8,
+  shopify,
+  pay,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  agent,
+  whyimage,
+  how1,
+  brands,
+  star,
+  faqs,
+  c,
+
   
 };
